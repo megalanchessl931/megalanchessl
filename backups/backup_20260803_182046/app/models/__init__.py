f0be4594f30f3f381b57ff725991460d079b78d3
megalanchessl931 <megalanchessl931@gmail.com>
2026-08-04 01:46:49 -0300

    Delete backups directory
    exclusao de todos os backups

--- backups/backup_20260803_182046/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from ..extensions import db
-from .user import User
-from .product import Product
-from .client import Client
-from .order import Order
-from .order_item import OrderItem
-
-__all__ = ["db", "User", "Product", "Client", "Order", "OrderItem"]
